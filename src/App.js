4607cf1f8f7d1a8557cba23a4e37504b2b0df889
Liew Chen Tzer <liewct115@gmail.com>
2023-01-30 15:18:32 +0800

    [UPDATE] 1) State

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
+import { useState,} from 'react';
 import './App.css';
 
-const Person = (person) =>{
-  return(
-    <>
-    <h1>Name: {person.name}</h1>
-    <h2>Last Name: {person.lastName}</h2>
-    <h2>Age: {person.age}</h2>
-    </>
-  )
-}
-
 const App=()=> {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-        <Person name = 'John' lastName = "Doe" age = {21}/>
-        <Person name = "Jane" lastName = "Liew" age = {21 +6}/>
-        <Person/>
+      <button onClick={()=>setCounter((prevCount) => prevCount -1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={()=>setCounter((prevCount) => prevCount +1)}>+</button>
     </div>
   );
 }
