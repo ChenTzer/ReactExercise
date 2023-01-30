c2056c29896edd95f01e7c5bba64561ca41ea3e7
Liew Chen Tzer <liewct115@gmail.com>
2023-01-30 14:53:19 +0800

    [UPDATE] 1) Tips text file 2)  React Fragment (<> </>) 3) If else 4) Constant (Variable)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
 const App=()=> {
+  const name = 'John';
+  const isNameShowing = true;
   return (
     <div className="App">
-        <h1>Hello, React!</h1>
+        <h1>Hello, {isNameShowing ? name : 'Nyes'}!</h1>
+        {name ? (
+          <>
+          test
+          </>
+        ): (
+          <>
+          <h1>test</h1>
+          <h2>There is no name</h2>
+          </>
+          
+        )}
     </div>
   );
 }
