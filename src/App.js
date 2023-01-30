54ceca6042167ce889f6de8672c794db47b410fa
Liew Chen Tzer <liewct115@gmail.com>
2023-01-30 15:06:29 +0800

    [UPDATE] 1) Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-const Person = () =>{
+const Person = (person) =>{
   return(
     <>
-    <h1>Name: John</h1>
-    <h2>Last Name: Doe</h2>
-    <h2>Age: 21</h2>
+    <h1>Name: {person.name}</h1>
+    <h2>Last Name: {person.lastName}</h2>
+    <h2>Age: {person.age}</h2>
     </>
   )
 }
@@ -13,6 +13,8 @@ const Person = () =>{
 const App=()=> {
   return (
     <div className="App">
+        <Person name = 'John' lastName = "Doe" age = {21}/>
+        <Person name = "Jane" lastName = "Liew" age = {21 +6}/>
         <Person/>
     </div>
   );
