727b2ce0f8feabb4b3ff910d810c164c1188945e
Liew Chen Tzer <liewct115@gmail.com>
2024-03-28 00:28:35 +0800

    LESSON 3 1) Inline css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Header"
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
-import Button from "./Button/Button";
+import Button from "./Button";
 
 function App() {
   return(
