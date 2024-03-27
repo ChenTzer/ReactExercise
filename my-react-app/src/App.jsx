cb5ec1c75c4e083cddece6b8265b2425cb5bef12
Liew Chen Tzer <liewct115@gmail.com>
2024-03-28 00:21:21 +0800

    LESSON 3 1) External Css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,12 +2,12 @@ import Header from "./Header"
 import Footer from "./Footer";
 import Food from "./Food";
 import Card from "./Card";
+import Button from "./Button";
 
 function App() {
   return(
       <>
-        <Card></Card>
-        <Card></Card>
+        <Button></Button>
       </>
   );
 
