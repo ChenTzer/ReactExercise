1f3332b0fc18601b7ced2b48dfb42801f02358aa
Liew Chen Tzer <liewct115@gmail.com>
2024-03-31 18:03:41 +0800

    LESSON 5 1) Conditional Rendering

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,17 +1,10 @@
-import Header from "./Header"
-import Footer from "./Footer";
-import Food from "./Food";
-import Card from "./Card";
-import Button from "./Button";
-import Student from "./Student";
+import UserGreeting from "./UserGreeting";
 
 function App() {
   return(
     <>
-      <Student name="Chen Tzer" age={24} isStudent={true}></Student>
-      <Student name="Jason" age={27} isStudent={false}></Student>
-      <Student name={30} age={27} isStudent={false}></Student>
-      <Student></Student>
+      <UserGreeting isLoggedIn={true} username="ChenTzer"></UserGreeting>
+      <UserGreeting></UserGreeting>
     </>
   );
 
