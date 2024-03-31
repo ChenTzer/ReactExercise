95e103763c67c8c65fd4b4fd2bef27727c9082e7
Liew Chen Tzer <liewct115@gmail.com>
2024-03-31 18:42:32 +0800

    LESSON 6 1) Render Lists

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,24 @@
-import UserGreeting from "./UserGreeting";
+import List from "./List";
 
 function App() {
+  const fruits = [{id: 1, name: "apple", calories: 95},
+                  {id: 2, name: "orange", calories: 45},
+                  {id: 3, name: "banana", calories: 105},
+                  {id: 4, name: "coconut", calories: 159},
+                  {id: 5, name: "pineapple", calories: 37}]
+
+  const vegetables = [{id: 6, name: "bok choy", calories: 45},
+                      {id: 7, name: "potato", calories: 110},
+                      {id: 8, name: "carrots", calories: 25},
+                      {id: 9, name: "corn", calories: 63},
+                      {id: 10, name: "brocolli", calories: 50}]
+
+  
+
   return(
     <>
-      <UserGreeting isLoggedIn={true} username="ChenTzer"></UserGreeting>
-      <UserGreeting></UserGreeting>
+      {fruits.length > 0&& <List items = {fruits} category="Fruits"></List>}
+      {vegetables.length > 0 && <List items = {vegetables} category="Vegetables"></List>}
     </>
   );
 
