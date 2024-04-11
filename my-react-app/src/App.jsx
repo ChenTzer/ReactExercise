6d760b2bbf247f3569477b0ce72b335ee80b83d7
Liew Chen Tzer <liewct115@gmail.com>
2024-04-11 20:35:47 +0800

    Lesson 10 1) Color Picker Exercise

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,9 @@
-import MyComponent from "./MyComponent";
-import Lesson8Counter from "./Lesson8Counter";
-import Lesson9MyComponent from "./Lesson9MyComponents";
+import ColorPicker from "./ColorPicker";
 
 function App() {
 
   return (
-      <Lesson9MyComponent></Lesson9MyComponent>
+      <ColorPicker></ColorPicker>
   );
 
 }
