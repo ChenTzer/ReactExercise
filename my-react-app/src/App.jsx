f88eabcfae19d2cdde810dc9e1d87cb85ec8f37d
Liew Chen Tzer <liewct115@gmail.com>
2024-04-14 17:14:16 +0800

    Lesson 19 1) useRef

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,10 @@
-import Lesson18ComponentA from './Lesson18ComponentA';
-import Lesson18ComponentB from './Lesson18ComponentB';
-import Lesson18ComponentC from './Lesson18ComponentC';
-import Lesson18ComponentD from './Lesson18ComponentD';
 import React from 'react';
+import Lesson19MyComponent from './Lesson19MyComponent';
 
 function App() {
 
   return (
-    <Lesson18ComponentA></Lesson18ComponentA>
+    <Lesson19MyComponent></Lesson19MyComponent>
   );
 
 }
---
+++ my-react-app/src/Lesson19MyComponent.jsx
@@ -0,0 +1,49 @@
+import React, {useState, useEffect, useRef} from 'react';
+
+function Lesson19MyComponent(){
+
+    const inputRef1 = useRef(0);
+    const inputRef2 = useRef(0);
+    const inputRef3 = useRef(0);
+
+    useEffect (() => {
+            console.log("COMPONENT RENDERED")
+        }
+    )   
+
+    function handleClick1(){
+        inputRef1.current.focus();
+        inputRef1.current.style.backgroundColor ="yellow";
+        inputRef2.current.style.backgroundColor ="";
+        inputRef3.current.style.backgroundColor ="";
+    }
+
+    function handleClick2(){
+        inputRef2.current.focus();
+        inputRef2.current.style.backgroundColor ="yellow";
+        inputRef1.current.style.backgroundColor ="";
+        inputRef3.current.style.backgroundColor ="";
+    }
+
+    function handleClick3(){
+        inputRef3.current.focus();
+        inputRef3.current.style.backgroundColor ="yellow";
+        inputRef1.current.style.backgroundColor ="";
+        inputRef2.current.style.backgroundColor ="";
+    }
+
+    return(
+        <div>
+            <button onClick={handleClick1}>Click me!</button>
+            <input ref={inputRef1}/>
+            
+            <button onClick={handleClick2}>Click me!</button>
+            <input ref={inputRef2}/>
+
+            <button onClick={handleClick3}>Click me!</button>
+            <input ref={inputRef3}/>
+        </div>
+    );
+}
+
+export default Lesson19MyComponent
